34c1250f9c78e872c3a6d2976e4cab6ccbc4305c
AlessonCarlos <alesson.carlos5@gmail.com>
2026-01-13 16:05:06 -0300

    Atualização do postagem.service e postagem.controlle com a implementação dos metodos do crud

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { PostagemService } from "../services/postagem.service";
 import { Postagem } from "../entities/postagem.entity";
 
-
+// responde o endpoint do insominia para receber as requisições
 @Controller("/postagens")
 export class PostagemController {
     constructor(private readonly postagemService: PostagemService) { }
@@ -12,5 +12,39 @@ export class PostagemController {
     findAll(): Promise<Postagem[]> {
         return this.postagemService.findall();
         
+    }
+
+    //vai ter um numero apos a postagem, que sera chamado pela id
+    //quando o metodo envovler um id especifico, a base sempre será essa, so mudará a logica
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id:number): Promise<Postagem>{
+        return this.postagemService.findById(id);
+    }
+
+    @Get('/titulo/:titulo')
+    @HttpCode(HttpStatus.OK)
+    findByAllTitulo(@Param('titulo') titulo: string): Promise<Postagem[]>{
+        return this.postagemService.findAllByTitulo(titulo)
+    }
+
+    //o get busca e o post ele cria
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body()postagem: Postagem): Promise<Postagem>{
+
+        return this.postagemService.create(postagem);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() postagem: Postagem): Promise<Postagem>{
+        return this.postagemService.update(postagem);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number){
+        return this.postagemService.delete(id)
     }
 }
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Postagem } from "../entities/postagem.entity";
-import { Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 
 @Injectable()
 export class PostagemService {
@@ -13,4 +13,62 @@ export class PostagemService {
     async findall(): Promise<Postagem[]> {
         return await this.postagemRepository.find();
     }
+
+    //Metodo para buscar postagens
+    //começa com async pois não é extantanel, o metodo precisa aguardar ser requisitado
+    // get por ID
+    async findById(id: number): Promise<Postagem> {
+        // Procura no banco de dados pela postagem e guarda o resultado na variavel
+        const postagem = await this.postagemRepository.findOne({
+            where: {
+                id,
+            },
+        });
+
+
+        // se não achou a postagem alie m cima, da um erro "Não encontrado" pro usuario
+        if(!postagem){
+
+            throw new HttpException('A postagem não foi encontra', HttpStatus.NOT_FOUND);
+        }
+
+        // se achoua  postagem la em cima, devolver ela pro usuario
+        return postagem;
+            
+    }
+    //Metodo para titulo
+    async findAllByTitulo(titulo: string): Promise<Postagem[]>{
+        return await this.postagemRepository.find({
+            where:{
+                titulo: ILike(`%${titulo}%`)
+            }
+        })
+    }
+
+    //Método para criar postagem
+    async create(postagem: Postagem): Promise<Postagem> {
+        return await this.postagemRepository.save(postagem);
+    }
+
+    //Método para atualizar uma postagem
+    async update(postagem: Postagem): Promise<Postagem>{
+
+        await this.findById(postagem.id)
+
+        //se deu tudo certo e a postagem foi encontrada o metodo save() entra em ação e salva a mudança
+        //No comando return, o await também é utilizado, indicando que a execução aguardará a conclusão da atualização antes de retornar a resposta para a classe que chamou o método.
+        return await this.postagemRepository.save(postagem);
+    }
+
+    //Método para deletar Postagem
+    async delete(id: number): Promise<DeleteResult>{
+
+        await this.findById(id)
+
+        return await this.postagemRepository.delete(id)
+    }
+
 }
+
+    
+
